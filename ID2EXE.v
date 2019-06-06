ff1ee9f248671e5bf0a80ff639a2c6d9816023b0
7smd7 <32780546+7smd7@users.noreply.github.com>
2019-06-06 11:23:29 +0430

    ID2EXE

--- ID2EXE.v
+++ ID2EXE.v
@@ -12,6 +12,19 @@ module ID2EXE (clk, rst, signExIn,  readData1In,  readData2In,  PC_In,  EXE_In, 
   output [31:0] signExOut, PC_Out, readData1Out, readData2Out;
 
   always @(posedge clk) begin
-    
+    if (rst) begin
+      {M_Out, WB_Out, EXE_Out, signExOut, PC_Out, readData1Out, readData2Out, src1_out, src2_out} <= 0;
+    end
+    else begin
+      M_Out <= M_In
+      WB_Out <= WB_In
+      EXE_Out <= EXE_In
+      signExOut <= signExIn
+      readData1Out <= readData1In;
+      readData2Out <= readData2In;
+      PC_Out <= PCIn;
+      src1_out <= src1_in;
+      src2_out <= src2_in;
+    end
   end
 endmodule // ID2EXE
